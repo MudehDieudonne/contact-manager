666a71fe6872a95530a02a67733f49ad4ba0088e
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-03-09 14:18:30 +0100

    Context.jsx

---
+++ src/context/ContactContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react"
+import { v4 as uuidv4 } from "uuid"
+
+const ContactContext = createContext()
+
+export const useContacts = () => useContext(ContactContext)
+
+export const ContactProvider = ({ children }) => {
+    const [contacts, setContacts] = useState([])
+
+    //Function to add Contacts
+    const addContact = (contact) => {
+        setContacts([...contacts, {id: uuidv4(), ...contact}])
+    }
+
+    //Edit Contact
+    const editContact = (updatedContact) => {
+        setContacts(
+            contacts.map((contact) => contact.id === updatedContact.id ? updatedContact : contact)
+        )
+    }
+
+    // Delete Function
+    const deleteContact = (id) => {
+        setContacts(contacts.filter((contact) => contact.id !== id))
+    }
+
+    return (
+        <ContactContext.Provider value={{contacts, addContact, editContact, deleteContact}}>
+            {children}
+        </ContactContext.Provider>
+        
+    )
+}
