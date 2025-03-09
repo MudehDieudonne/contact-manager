fba168ad4e10516618ecc61985b72f9da3309bad
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-03-09 13:47:38 +0100

    Basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>App Mian</h2>
+      <button className='btn btn-success me-2'>
+        <i className='fa fa-home' />Submit
+      </button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+/* Fontawsome icons */
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
+/*bootstrap */
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
